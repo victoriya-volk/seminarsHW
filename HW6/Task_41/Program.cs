b09b4679636a18ed81242249cb0ac56e155d2964
Victoriya Volk <victoriyavolk@icloud.com>
2022-09-16 21:13:29 +0300

    HW6

---
+++ HW6/Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+// пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+void PositiveNumbers (int num)
+{
+    int count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine("Bведите число:");
+        int newNum = Convert.ToInt32(Console.ReadLine());
+        if (newNum > 0) count += 1;
+    }
+    Console.WriteLine($"{count} чисел больше 0");
+}
+
+Console.WriteLine("Введите количетсво чисел для проверки:");
+int numM = Convert.ToInt32(Console.ReadLine());
+PositiveNumbers(numM);
---
+++ HW6/Task_43/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void CrossingPoint (float b1, float k1, float b2, float k2)
+{
+    float pointX =(b2-b1)/(k1-k2);
+    float pointY = k1 * pointX + b1;
+    Console.WriteLine($"{Math.Round(pointX, 1)}, {Math.Round(pointY,1)}");
+}
+
+CrossingPoint(2, 5, 4, 9);
+CrossingPoint(-1, 2, 1, -3);
